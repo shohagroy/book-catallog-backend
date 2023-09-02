3051ef57d3273e8dd8b7622a1d639e3ffd85752c
shohag roy <pkshohag240@gmail.com>
2023-09-03 00:06:46 +0600

    global error handelar added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import httpStatus from "http-status";
 import mainRoute from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/v1", mainRoute);
+app.use(globalErrorHandler);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,14 +1,15 @@
+import httpStatus from "http-status";
+import ApiError from "../../../errors/ApiError";
 import { hashedPassword } from "../../utils/hashedPassword";
 import { jwtHelpers } from "../../utils/jwtHelpers";
 import { IUser } from "../user/user.interface";
 import { userService } from "../user/user.service";
 
 const createNewUser = async (payload: IUser) => {
-  console.log(payload);
   const isUserExists = await userService.findByEmail(payload.email);
 
   if (isUserExists) {
-    console.log("user already exists");
+    throw new ApiError(httpStatus.FORBIDDEN, "user already exists");
   }
 
   payload.password = await hashedPassword.createhas(payload.password!);
@@ -16,13 +17,9 @@ const createNewUser = async (payload: IUser) => {
 
   const token = await jwtHelpers.createToken(newUser);
 
-  console.log(token, newUser);
-
   newUser.password = null;
 
   return { user: newUser, token };
-
-  // return isUserExists;
 };
 
 export const authService = {
